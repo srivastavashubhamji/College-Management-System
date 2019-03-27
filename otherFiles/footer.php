6ebd844a41617d640c57c3f47fb05a1d74306f96
srivastavashubhamji <38492475+srivastavashubhamji@users.noreply.github.com>
2019-03-27 04:33:01 -0700

    footer for all pages

---
+++ otherFiles/footer.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<!-- ------ Head Section ------ -->
+<head>
+    <meta charset="UTF-8">
+    <title>Footer ji</title>
+    <link rel="stylesheet" href="./cssFooter1.css">
+    <link rel="stylesheet" href="./css/all.css">
+</head>
+<!-- ------ Body Section ------ -->
+<body>
+    <h2>Footer Demo</h2>
+    <hr>
+    <div class="footMain">
+    <!-- ------ Social Media Section ------ -->
+            <div class="ftr social">
+                <p>Social Media</p>
+                <div class="socialCont">
+                    <img src="./images/logo-facebook.png" alt="Facebook">
+                    <img src="./images/logo-instagram.png" alt="Instagram">
+                    <img src="./images/logo-linkedin.png" alt="Linked In">
+                    <img src="./images/logo-whatsapp.png" alt="WhatsApp">
+                    <img src="./images/logo-skype.png" alt="Skype">
+                    <img src="./images/logo-viber.png" alt="Viber">
+                </div>
+            </div>            
+    <!-- ------ Latest Plasements Section ------ -->            
+        <div class="ftr place">
+            <p>Placements</p>
+            <div class="placeCont">
+                <img src="./images/campus-badoo.png" alt="Byju's">
+                <div>Byju's Inc. <span class="ftrPlcTitles">Pvt. Lmt.</span></div>
+                <img src="./images/campus-xing.png" alt="Xylo">
+                <div>Xylo Inc.<span class="ftrPlcTitles">Pvt. Lmt.</span></div>
+                <img src="./images/campus-shubh.png" alt="Shubh Company">
+                <div>Shubh Pvt.<span class="ftrPlcTitles"> Lmt.</span></div>
+                <img src="./images/campus-deviantart.png" alt="Devilian">
+                <div>Amazon <span class="ftrPlcTitles">Pvt. Lmt.</span></div>
+            </div>
+        </div>
+	</div>
+</body>
+</html>
