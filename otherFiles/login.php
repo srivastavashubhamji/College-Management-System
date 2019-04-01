2f5864006ed6680acbfc2abc31c7a0ee45a707e6
srivastavashubhamji <38492475+srivastavashubhamji@users.noreply.github.com>
2019-04-01 09:26:46 -0700

    Updated Login page (Complete)

--- otherFiles/login.php
+++ otherFiles/login.php
@@ -1,36 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
-
+<!-- ---------------- HEAD SECTION START ----------------- -->
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
+    <link rel="stylesheet" href="../css/all.css">
     <link rel="stylesheet" href="../css/style.css">
+    <script src="../javascript/main.js"></script>
+    <script src="../javascript/sweetalertfile.js"></script>
 </head>
-
-<body class="loginBody">
-    <form action="login.php" method="post">
+<!-- --------------- BODY SECTION START ------------------ -->
+<body>
+    <form action="./login.php" method="post">       <!-- FORM  -->
         <div class="mainLogin">
             <h3 class="linHead">Login</h3>
+            <a href="../index.html"><i class="crecross">&times;</i></a>            
             <div class="Login">
-                <input type="text" name="username" placeholder="Enter Username" class="loginInput">
-                <input type="Password" name="password" placeholder="Enter Password" class="loginInput">
-                <input type="submit" value="Submit" class="input btn">
+                <input type="text" name="username" placeholder="Enter Username" class="input" required>                
+                <input type="Password" name="password" placeholder="Enter Password" class="input" id="loginPass" required>
+                
+                <span class="logspa"><i class="seeLpass far fa-eye-slash" id="showLogPass" onclick="showPasss('1')"></i></span>
+                
+                <input type="submit" name="submit" value="Submit" class="input btn">
                 <div class="remMe">
                     <input type="checkbox" class="rem" id="rem"> <label for="rem" class="loginLabel">Remember Me</label>
                 </div>
+
                 <div class="loginFoot">
                     <a href="#forgetPass" class="loginA">Forget Password ?</a>
-                    <a href="./createNew.html" class="loginB">Create New A/C</a>
+                    <a href="../webpages/create.php" class="loginB">Create New A/C</a>
                 </div>
             </div>
         </div>
     </form>
 </body>
-
 </html>
-<!--- php coding start --->
+<!-- -------------- PHP SECTION START ---------------->
 <?php
-
-
-
+    include("./dbconn.php");    // DATABASE CONNECTED... //
+    if(isset($_POST["submit"])) // SUBMIT CLICKED...     //
+    {
+        $uname = $_POST["username"];
+        $pass = $_POST["password"];
+        $sql="SELECT * FROM `user_create` WHERE `username`='$uname' and `password`='$pass'";
+        $run = mysqli_query($conn,$sql);    // FETCHING THE RECORD WHICH MAY CONTAINS THE RECORD ACCORDING TO UNAME AND PASS //
+        $result = mysqli_num_rows($run);    // COUNTING THE FETHCED RECORD ( IF USERNAME AND PASSWORD ARE MATCHED )//           
+        if($result < 1)                     // THAT MEANS UNAME OR PASS NOT FOUND IN DATABASE
+        {   
+            ?>           
+            <script>                
+                swal({
+                    title: "OOPs",
+                    text: "Username or Password Not found",
+                    icon: "error"
+                });
+            </script>
+            <?php
+        }
+        else
+        {
+            header('location:../index.html');
+            ?>
+            <script>
+                swal({
+                    title: "Congratulations.",
+                    text: "Login Successfully",
+                    icon: "success"
+                });
+            </script>            
+            <?php
+        }
+    }
 ?>
