190073260459a9cb7129e20312bc9ee946941bba
shubham srivastava <shubhsrivastava271999@gmail.com>
2019-09-23 10:02:16 -0700

    Added fees Zone file for Manager accessibility

---
+++ webpages/adminshubh786/manager/feesZone.php
@@ -0,0 +1,8 @@
+<?php
+    include 'header.php';
+?>
+
+    <br><br><br><br><br><h2>Welcome in Fees Zone</h2><hr><br><br><br><br><br>
+    <script src="../../../javascript/main.js"></script>
+    <script src="../../../javascript/sweetalertfile.js"></script>
+</body><!-- opening <body> tag is in 'header.php' -->
