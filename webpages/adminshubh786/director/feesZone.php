5bd24a0c1d234272f92627bc3b825cb34b8ee090
shubham srivastava <shubhsrivastava271999@gmail.com>
2019-09-23 09:42:59 -0700

    Added Fees Zone information status file for Director

---
+++ webpages/adminshubh786/director/feesZone.php
@@ -0,0 +1,9 @@
+<?php
+    include 'header.php';
+?>
+
+<br><br><br><br><br><h2>Welcome in Fees Zone</h2><hr><br><br><br><br><br>
+
+</body>
+<script src="../../../javascript/main.js"></script>
+<script src="../../../javascript/sweetalertfile.js"></script>
