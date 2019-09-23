a7a247352990b56b0193f70a7d178af3c555414c
shubham srivastava <shubhsrivastava271999@gmail.com>
2019-09-22 17:55:26 -0700

    Added media.php files to show the best memories of the college

---
+++ webpages/media.php
@@ -0,0 +1,17 @@
+<?php
+//  Including the Logo and the Navigation bar : Header...
+    include 'header.php';
+?>
+<div class="mainMedia">
+    <img id="img" src="../images/mediaPics/1.jpg?12" alt="Shubham Srivastava">
+    <b><p id="msg"></p></b>
+</div>
+<?php
+//  Including the footer for the page : Footer ...
+    include 'footer.php';
+?>
+<body>
+    <script>
+        startScreenPlay();
+    </script>
+</body>
