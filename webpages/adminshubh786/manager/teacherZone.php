c2020464c5f8544dd007eb0fb2dad9af47f7f61b
shubham srivastava <shubhsrivastava271999@gmail.com>
2019-09-23 10:03:59 -0700

    Added teachers zone file for Manager to manage staff of the college

---
+++ webpages/adminshubh786/manager/teacherZone.php
@@ -0,0 +1,10 @@
+<?php
+    include 'header.php';
+?>
+
+    <br><br><br><br><br><h2>Welcome in Teachers Zone</h2><hr><br><br><br><br><br>
+
+    
+    <script src="../../../javascript/main.js"></script>
+    <script src="../../../javascript/sweetalertfile.js"></script>
+</body><!-- opening <body> tag is in 'header.php' -->
