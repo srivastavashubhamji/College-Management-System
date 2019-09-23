f4108ee8303d35be00c755d64a519f6f9a77ab43
shubham srivastava <shubhsrivastava271999@gmail.com>
2019-09-23 10:03:20 -0700

    Added Student Zone file for manager to manage student of college

---
+++ webpages/adminshubh786/manager/studentZone.php
@@ -0,0 +1,12 @@
+<?php
+    include 'header.php';
+?>
+
+    <br><br><br><br><br><h2>Welcome in Student Zone</h2><hr><br><br><br><br><br>
+
+
+
+
+    <script src="../../../javascript/main.js"></script>
+    <script src="../../../javascript/sweetalertfile.js"></script>
+</body><!-- opening <body> tag is in 'header.php' -->
